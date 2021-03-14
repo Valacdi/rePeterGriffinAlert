ae84c403e46ccb59424da60ab9796848636e4d1a
jrwparrott <80632367+jrwparrott@users.noreply.github.com>
2021-03-14 15:14:11 +0000

    Add files via upload

---
+++ Peter Griffin Alert/frmPeterMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peter_Griffin_Alert
+{
+    public partial class frmPeterMain : Form
+    {
+        public frmPeterMain()
+        {
+            InitializeComponent();
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
